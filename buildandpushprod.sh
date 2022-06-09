f47044eeb752fa2e365784972b9e8c1d3499f1c8
whibbard <whibbard@estee.com>
2022-06-08 14:20:43 -1000

    initial commit

---
+++ buildandpushprod.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker build -t hippy96/samplewebapp:latest .
+docker push hippy96/samplewebapp:latest
---
+++ buildandpushtest.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker build -t hippy96/samplewebapp:test .
+docker push hippy96/samplewebapp:test
